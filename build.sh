13d8cbb9f032f013d9e66de292e6a43ec8c9ec0d
richard.thomas <richard.thomas@interfront.co.za>
2018-05-10 11:06:44 +0200

    added a style shet

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #/bin/sh
 
+rm -r dist
 mkdir dist
-elm-make ./src/Main.elm --output dist/main.js
+mkdir dist/js
+elm-make ./src/Main.elm --output dist/js/main.js
 cp *.html dist/
 cp -r src/css dist/
 cp -r src/assets dist/
