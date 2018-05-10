0b3781c79eba22b82822372f12d14432f0ca3a8c
richard.thomas <richard.thomas@interfront.co.za>
2018-05-10 10:37:31 +0200

    made the homepage folder

---
+++ src/HomePage/Update.elm
@@ -0,0 +1,18 @@
+module Login.Update exposing (..)
+
+import Login.Model exposing (..)
+
+
+type Msg
+    = UsernameInput String
+    | PasswordInput String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        UsernameInput username ->
+            { model | username = username }
+
+        PasswordInput password ->
+            { model | password = password }
